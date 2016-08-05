a38f3b9d2ba5f047d206a7437eb0a6296049d278
timejiawei <timejiawei@gmail.com>
2016-08-05 22:13:10 +0800

    New project

---
+++ JWCodeGeneration/JWCodeGeneration/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  JWCodeGeneration
+//
+//  Created by zhangjiaweiv on 16/8/5.
+//  Copyright © 2016年 timejiawei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
